a282648b70ad00e1ae18e6cf4978c888b9df168a
fabio.dorea <fabio.dorea@evoluum.com.br>
2021-05-18 22:55:13 -0300

    Remove comentários.

--- src/main/java/com/sinqia/sqspii/service/SignQrCodeService.java
+++ src/main/java/com/sinqia/sqspii/service/SignQrCodeService.java
@@ -10,7 +10,6 @@ import com.sinqia.sqspii.entity.VaultPrivateKey;
 import com.sinqia.sqspii.repository.ParameterRepository;
 import com.sinqia.sqspii.repository.PrivateKeyRepository;
 import com.sinqia.sqspii.response.PrivateKeyResponse;
-import com.sun.org.apache.xml.internal.security.algorithms.SignatureAlgorithm;
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,20 +20,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.ByteArrayInputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.security.KeyFactory;
-import java.security.PrivateKey;
-import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-import java.security.spec.PKCS8EncodedKeySpec;
 
 @Service
 public class SignQrCodeService {
 
-    private static final String JWK_URL = "localhost:8100/sqspii/.well-known/jwks.json";
+    @Value("${pix.well-know.url}")
+    private String JWK_URL;
 
     @Value("${sinqia.vault.host}")
     private String vaultHost;
@@ -61,15 +54,6 @@ public class SignQrCodeService {
 
         VaultPrivateKey pk = privateKeyRepository.findPrivateKey(parameter.getIdEntPar()).
                 orElseThrow(() -> new RuntimeException("Nenhuma chave privada encontrada para este cliente: " + TenantContext.getCurrentTenant()));
-
-
-        //X509Certificate certificate = parseCertificate(pk.getDigitalCertificate().getDescriptionCertificate());
-
-       /* Signature sign = Signature.getInstance("SHA1withRSA");
-        sign.initSign(getPrivateKey(parameter.getIdEntPar(), pk.getId()));
-        sign.update(payload.getBytes(StandardCharsets.UTF_8));
-        String result = new String(Base64.encodeBase64(sign.sign()), StandardCharsets.UTF_8);
-        return result;*/
 
         return signPayloadWithNimbus(payload, pk.getDigitalCertificate().getId(), getPrivateKey(parameter.getIdEntPar(), pk.getId()));
     }
@@ -113,13 +97,6 @@ public class SignQrCodeService {
             return "-----BEGIN PRIVATE KEY-----\n" +
                     response.getBody().getData().getData().getChave() +
                     "\n-----END PRIVATE KEY-----";
-            /*Base64 b64 = new Base64();
-            byte [] decoded = b64.decode(privateKey);
-
-            PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decoded);
-            KeyFactory kf = KeyFactory.getInstance("RSA");
-            return kf.generatePrivate(spec);*/
-            //return "-----BEGIN PRIVATE KEY-----\n" + privateKey + "\n-----END PRIVATE KEY-----";
         }
         return null;
     }
